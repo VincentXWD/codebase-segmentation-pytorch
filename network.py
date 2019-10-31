9b0aba07fd80078dde1b14ba88bef286f18fd3d6
vincentxwd <kirai.wendong@gmail.com>
2019-10-31 20:51:49 +0800

    resolve some bugs

--- network.py
+++ network.py
@@ -2,6 +2,7 @@ from models import Unet
 
 
 def get_model():
+  # TODO(xwd): add flags for all entries.
   return Unet(
       encoder_name='dilated_resnet101',
       encoder_weights='imagenet',
--- train.py
+++ train.py
@@ -1,10 +1,25 @@
 import torch
 
 import network
+from misc.sync_batchnorm.batchnorm import convert_model
 
 from utils.transforms import Compose, Scale, RandomRotate, RandomHorizontallyFlip
 
 
-if __name__ == '__main__':
+def train_epoch():
+  pass
+
+
+def train():
+  pass
+
+
+def main():
   augmentations = Compose([Scale(2048), RandomRotate(10), RandomHorizontallyFlip(0.5)])
   model = network.get_model()
+  # TODO(xwd): add flag [convert_sync_bn]
+  model = convert_model(model)
+
+
+if __name__ == '__main__':
+  main()
