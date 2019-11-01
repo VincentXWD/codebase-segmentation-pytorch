bcde53a584844754529a7614ea3c3b62dbb24799
vincentxwd <kirai.wendong@gmail.com>
2019-11-01 15:23:53 +0800

    remove abundant files

--- utils/transforms/flips.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def flip_horizon(image, label, prob):
-  if prob:
-    if np.random.choice([False, True], size=(), p=[1 - prob, prob]):
-      image = np.flip(image, axis=1)
-      label = np.flip(label, axis=1)
-  return image, label
